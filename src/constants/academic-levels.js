f585e10ef2c05e46fa45360c55a67df2075a0069
Miqueias Fernandes <miqueias.fernandes@bitfy.app>
2023-02-23 22:25:27 -0300

    fix: set id values as same "name" label values

--- src/constants/academic-levels.js
+++ src/constants/academic-levels.js
@@ -1,8 +1,8 @@
 export default [
-  { id: 1, name: 'Ensino Fundamental Completo' },
-  { id: 2, name: 'Ensino Fundamental Incompleto' },
-  { id: 3, name: 'Ensino Médio Completo' },
-  { id: 4, name: 'Ensino Médio Incompleto' },
-  { id: 5, name: 'Ensino Superior Completo' },
-  { id: 6, name: 'Ensino Superior Incompleto' },
+  { id: 'Ensino Fundamental Complet', name: 'Ensino Fundamental Completo' },
+  { id: 'Ensino Fundamental Incompleto', name: 'Ensino Fundamental Incompleto' },
+  { id: 'Ensino Médio Completo', name: 'Ensino Médio Completo' },
+  { id: 'Ensino Médio Incompleto', name: 'Ensino Médio Incompleto' },
+  { id: 'Ensino Superior Completo', name: 'Ensino Superior Completo' },
+  { id: 'Ensino Superior Incompleto', name: 'Ensino Superior Incompleto' },
 ];
