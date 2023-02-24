f2ec6c38d46ceecdc2a8f95664ca7353611caa8b
Miqueias Fernandes <miqueias.fernandes@bitfy.app>
2023-02-23 22:26:15 -0300

    fix: ensure show componente is render correctly

--- src/components/members/show.jsx
+++ src/components/members/show.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { SimpleShowLayout, Show, TextField } from 'react-admin';
+import { FunctionField, Show, SimpleShowLayout } from 'react-admin';
 import columns from '../../constants/model-columns';
 
 export default function ShowMember() {
   return (
     <Show>
       <SimpleShowLayout>
-        {columns.full.map((item) => <TextField label={item.label} source={item.source} />)}
+        {columns.full.map((item) => <FunctionField label={item.label} source={item.source} render={(render) => render[item.source] || '-'} />)}
       </SimpleShowLayout>
     </Show>
   );
