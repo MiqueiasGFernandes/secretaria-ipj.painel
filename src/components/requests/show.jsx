bfb8eaee8c73b60874f26bdf8bb84ac95bdefa28
miqueias.fernandes@bitfy.app <miqueiasgomesfernandes@gmail.com>
2024-04-30 19:41:20 -0300

    feat: add member data show and aprove dialog

--- src/components/requests/show.jsx
+++ src/components/requests/show.jsx
@@ -1,17 +1,114 @@
-import React from 'react';
-import { Show, SimpleShowLayout } from 'react-admin';
-import columns from './request-model-columns';
+import { Close, Done } from '@mui/icons-material';
+import { Grid, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import {
+  Button,
+  Confirm,
+  FunctionField,
+  Show,
+  SimpleShowLayout,
+  useNotify,
+  useRecordContext,
+  useRedirect,
+  useUpdate,
+} from 'react-admin';
+import memberColumns from '../../constants/model-columns';
+import requestColumns from './request-model-columns';
+
+import isBoolean from '../../helpers/is-boolean';
+import isEmail from '../../helpers/is-email';
+
+function ConfirmDialogs({ dialogOpen, setDialogOpen }) {
+  const notify = useNotify();
+  const record = useRecordContext();
+
+  const redirect = useRedirect();
+  const [update] = useUpdate();
+
+  const handleConfirm = () => {
+    update('requests', { id: record.id, data: { status: 'approved' } })
+      .catch(() => {
+        notify('Ocorreu um erro interno ao aprovar o registro');
+      })
+      .then(() => {
+        notify('Solicitação aprovada com êxito');
+        redirect('/requests');
+      });
+  };
+
+  return (
+    <Confirm
+      isOpen={dialogOpen.APPROVE}
+      title="Aprovar solicitação"
+      content="Deseja aprovar a solicitação?"
+      cancel="Cancelar"
+      onConfirm={handleConfirm}
+      onClose={() => setDialogOpen({ ...dialogOpen, APPROVE: false })}
+    />
+  );
+}
 
 export default function ShowRequest() {
+  const [openDialog, setOpenDialog] = useState({
+    APPROVE: false,
+    REJECT: false,
+  });
+
   return (
-    <Show>
-      <SimpleShowLayout>
-        {columns.map((item) => React.createElement(item.View, {
-          label: item.label,
-          source: item.source,
-        }))}
-        {/* Adicionar botões de ação e estratégias de exibição de membro */}
-      </SimpleShowLayout>
-    </Show>
+    <>
+      <Show>
+        <ConfirmDialogs dialogOpen={openDialog} setDialogOpen={setOpenDialog} />
+        <SimpleShowLayout>
+          <Typography fontSize={32}>Dados da Solicitação</Typography>
+          {requestColumns.map((item) => React.createElement(item.View, {
+            label: item.label,
+            source: item.source,
+          }))}
+          <Grid xs={12} justifyItems="start">
+            <Button
+              color="success"
+              startIcon={<Done />}
+              variant="contained"
+              label="Aprovar"
+              style={{
+                marginRight: 5,
+              }}
+              onClick={() => setOpenDialog({ ...openDialog, APPROVE: true })}
+            />
+            <Button
+              color="error"
+              startIcon={<Close />}
+              variant="contained"
+              label="rejeitar"
+              onClick={() => setOpenDialog({ ...openDialog, REJECT: true })}
+            >
+              Rejeitar
+            </Button>
+          </Grid>
+        </SimpleShowLayout>
+      </Show>
+      <Show style={{ paddingBottom: 10 }}>
+        <SimpleShowLayout>
+          {memberColumns.full
+            .filter((item) => item.source !== 'memberDocumentId')
+            .map((item) => (
+              <FunctionField
+                label={item.label}
+                source={item.source}
+                render={({ member: render }) => {
+                  if (isBoolean(render[item.source])) {
+                    return item ? 'Sim' : 'Não';
+                  }
+
+                  if (isEmail(render[item.source])) {
+                    return <a title={`Enviar email para: ${render[item.source]}`} href={`mailto:${render[item.source]}`}>{render[item.source]}</a>;
+                  }
+                  return render[item.source] || '-';
+                }}
+              />
+            ))}
+        </SimpleShowLayout>
+      </Show>
+    </>
   );
 }
