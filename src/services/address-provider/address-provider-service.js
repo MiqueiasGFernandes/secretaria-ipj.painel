78cbb45fbc7cabaa233c04101566c593126624cd
miqueias.fernandes@bitfy.app <miqueiasgomesfernandes@gmail.com>
2024-05-29 10:25:19 -0300

    fix: solve CEP search bug

--- src/services/address-provider/address-provider-service.js
+++ src/services/address-provider/address-provider-service.js
@@ -3,7 +3,7 @@ import remoteAddressProvider from './remote-address-provider';
 let dataProvider = null;
 
 function setDataProvider() {
-  if (process.env.REACT_APP_USE_LOCAL_DATA) {
+  if (!process.env.REACT_APP_USE_LOCAL_DATA) {
     dataProvider = remoteAddressProvider;
   }
 }
