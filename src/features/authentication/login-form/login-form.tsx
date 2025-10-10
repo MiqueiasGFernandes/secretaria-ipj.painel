0410e634f70e5cf13d59554793a610cb5b536089
miqueias.fernandes@bitfy.app <miqueiasgomesfernandes@gmail.com>
2025-10-10 19:12:28 -0300

    fix: link show

--- src/features/authentication/login-form/login-form.tsx
+++ src/features/authentication/login-form/login-form.tsx
@@ -1,23 +1,26 @@
 import bg from "../../../assets/login-backgroud.png";
 
 import { BackgroudImageContainer } from "@components/background-image-container";
+import { Typography } from "@mui/material";
 import { Link, LoginForm as RaLoginForm } from "react-admin";
 
-export function LoginForm({ handleRedirectToSignUpForm}: { handleRedirectToSignUpForm: () => void}) {
+export function LoginForm({ handleRedirectToSignUpForm }: { handleRedirectToSignUpForm: () => void }) {
   return <div>
     <BackgroudImageContainer backgroundImage={bg}>
       <RaLoginForm />
-    </BackgroudImageContainer>    
-    <Link
-      to="/#"
-      onClick={handleRedirectToSignUpForm}
-      style={{
-        textAlign: 'center',
-        width: '100%',
-        display: 'block',
-      }}
-    >
-      Crie uma conta agora
-    </Link>
+      <Link
+        to="/#"
+        onClick={handleRedirectToSignUpForm}
+        style={{
+          textAlign: 'center',
+          width: '100%',
+          display: 'block',
+        }}
+      >
+        <Typography>
+          Crie uma conta agora
+        </Typography>
+      </Link>
+    </BackgroudImageContainer>
   </div>
 }
