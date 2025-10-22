0b9b0ba57bc574a4cdef713f6799d11e16fd91dd
miqueias.fernandes@bitfy.app <miqueiasgomesfernandes@gmail.com>
2025-10-22 16:36:17 -0300

    fix: add redirect when somenthing was wrong with auth

--- src/features/dashboard/dashboard.tsx
+++ src/features/dashboard/dashboard.tsx
@@ -1,10 +1,15 @@
 import { useMetrics } from "@features/hooks";
 import { MembersMetricPainel } from "./members-metric-painel";
+import { useRedirect } from "ra-core";
 
 export function Dashboard() {
   const { metrics, loading, error } = useMetrics();
+  const redirect = useRedirect()
 
   if (loading) return <p>Carregando métricas...</p>;
+  if ((error as any)?.status === 401) {
+    redirect("/login")
+  }
   if (error) return <p>Erro ao carregar métricas.</p>;
 
   return <MembersMetricPainel count={metrics} />;
